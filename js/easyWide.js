f55050229d0d1ead47b1572da713ba2ec85382e1
Nicolas Ribes <nicolas.ribes@sensio.com>
2016-05-04 18:44:26 +0200

    [Js] added css feature and maque js resize in fallback ie8

--- js/easyWide.js
+++ js/easyWide.js
@@ -10,6 +10,42 @@
       myOptions = $.extend(options, option);
     else
       myOptions = options;
+
+    var checkIe = function(){
+      var myNav = navigator.userAgent.toLowerCase();
+      return (myNav.indexOf('msie') != -1) ? parseInt(myNav.split('msie')[1]) : false;
+    };
+
+    var setCssFull = function (container, object) {
+        var src = object.attr("src");
+        var postion = ""
+        switch (myOptions.alignement) {
+          case 'center':
+            postion = "50% 50%";
+            break;
+          case 'top':
+            postion = "50% 0";
+            break;
+          case 'bottom':
+            postion = "50% 100%";
+            break;
+          case 'left':
+            postion = "0 50%";
+            break;
+          case 'right':
+            postion = "100% 50%";
+            break;
+          default:
+            postion = "50% 50%";
+        }
+        container.css({
+          backgroundSize:"cover",
+          backgroundPosition:postion,
+          backgroundImage:"url("+src+")"
+        })
+        object.hide()
+    }
+
     var setFullScreen = function (container, object) {
       object.attr('style', '');
       object.css({ 'position': 'absolute' });
@@ -23,29 +59,29 @@
         backgroundWidth = Math.round(backgroundHeight * contentRatio);
       }
       switch (myOptions.alignement) {
-      case 'center':
-        backgroundTop = (containerHeight - backgroundHeight) * 0.5;
-        backgroundLeft = (containerWidth - backgroundWidth) * 0.5;
-        break;
-      case 'top':
-        backgroundTop = 0;
-        backgroundLeft = (containerWidth - backgroundWidth) * 0.5;
-        break;
-      case 'bottom':
-        backgroundTop = containerHeight - backgroundHeight;
-        backgroundLeft = (containerWidth - backgroundWidth) * 0.5;
-        break;
-      case 'left':
-        backgroundTop = (containerHeight - backgroundHeight) * 0.5;
-        backgroundLeft = 0;
-        break;
-      case 'right':
-        backgroundTop = (containerHeight - backgroundHeight) * 0.5;
-        backgroundLeft = containerWidth - backgroundWidth;
-        break;
-      default:
-        backgroundTop = (containerHeight - backgroundHeight) * 0.5;
-        backgroundLeft = (containerWidth - backgroundWidth) * 0.5;
+        case 'center':
+          backgroundTop = (containerHeight - backgroundHeight) * 0.5;
+          backgroundLeft = (containerWidth - backgroundWidth) * 0.5;
+          break;
+        case 'top':
+          backgroundTop = 0;
+          backgroundLeft = (containerWidth - backgroundWidth) * 0.5;
+          break;
+        case 'bottom':
+          backgroundTop = containerHeight - backgroundHeight;
+          backgroundLeft = (containerWidth - backgroundWidth) * 0.5;
+          break;
+        case 'left':
+          backgroundTop = (containerHeight - backgroundHeight) * 0.5;
+          backgroundLeft = 0;
+          break;
+        case 'right':
+          backgroundTop = (containerHeight - backgroundHeight) * 0.5;
+          backgroundLeft = containerWidth - backgroundWidth;
+          break;
+        default:
+          backgroundTop = (containerHeight - backgroundHeight) * 0.5;
+          backgroundLeft = (containerWidth - backgroundWidth) * 0.5;
       }
       container.css({
         'position': 'relative',
@@ -59,6 +95,7 @@
         'width': backgroundWidth
       });
     };
+
     $(this).each(function (i) {
       var $this = $(this);
       var gloabalContainer;
@@ -70,31 +107,50 @@
         gloabalContainer = $this.parent();
       $this.load(function () {
         if (activate) {
-          setFullScreen(gloabalContainer, $this);
+          if ( checkIe () &&  checkIe() < 9) {
+            setFullScreen(gloabalContainer, $this);
+          }
         }
       });
-      setFullScreen(gloabalContainer, $this);
+      if ( checkIe () &&  checkIe() < 9) {
+        setFullScreen(gloabalContainer, $this);
+      }else {
+        setCssFull(gloabalContainer, $this);
+      }
       $(window).resize(function () {
         clearTimeout(myTimer);
         myTimer = setTimeout(function () {
           if (activate) {
-            setFullScreen(gloabalContainer, $this);
+            if ( checkIe () &&  checkIe() < 9) {
+              setFullScreen(gloabalContainer, $this);
+            }
           }
         }, 50);
       });
       var publicfunctions = {
         manualResize: function () {
           if (activate) {
-            setFullScreen(gloabalContainer, $this);
+            if ( checkIe () &&  checkIe() < 9) {
+              setFullScreen(gloabalContainer, $this);
+            }
           }
         },
         destroy: function () {
           activate = false;
-          $this.attr('style', '');
+          if ( checkIe () &&  checkIe() < 9) {
+            $this.attr('style', '');
+          }else {
+            gloabalContainer.attr('style', '');
+            $this.show()
+          }
         },
         reload: function () {
           activate = true;
-          setFullScreen(gloabalContainer, $this);
+          if ( checkIe () &&  checkIe() < 9) {
+            setFullScreen(gloabalContainer, $this);
+          } else {
+            setCssFull(gloabalContainer, $this);
+          }
         }
       };
       $this.data('easyWide', publicfunctions);
